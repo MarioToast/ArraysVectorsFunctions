42f119ac0dbd0631a59b38fc5da7f261fb7082d4
Christian F <70570052+MarioToast@users.noreply.github.com>
2020-09-15 10:04:33 +0200

    hjg

--- ArraysVectorsFunctions/ArraysVectorsFunctions.cpp
+++ ArraysVectorsFunctions/ArraysVectorsFunctions.cpp
@@ -4,7 +4,6 @@
 
 int main()
 {
-
     /*const int arraysize = 20;
     int myarrat[arraysize] = {};
 
@@ -12,8 +11,16 @@ int main()
     stdID[2] = 200;
     std::cout << stdID[50];*/
 
-    double num[5] = {};
-    std::cout << "Enter a number";
+    /*double num[5] = {};
+    std::cout << "Enter a number: ";
     std::cin >> num[0];
-    std::cout << num[0];
+    std::cout << num[0];*/
+
+    double me[100] = {};
+    std::cout << "Size of me is " << sizeof(me) / sizeof(me[0]) << "\n";
+
+    for (int i = 0; i < sizeof(me) / sizeof(me[0]); i++) {
+        std::cout << me[i] << " ";
+    }
+
 }
