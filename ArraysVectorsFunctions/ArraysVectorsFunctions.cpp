166914193aede1225f29a6c838ad6c891c06b233
Christian F <70570052+MarioToast@users.noreply.github.com>
2020-09-15 09:45:17 +0200

    Add project files.

---
+++ ArraysVectorsFunctions/ArraysVectorsFunctions.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+
+int main()
+{
+
+    /*const int arraysize = 20;
+    int myarrat[arraysize] = {};
+
+    int stdID[53] = {};
+    stdID[2] = 200;
+    std::cout << stdID[50];*/
+
+    double num[5] = {};
+    std::cout << "Enter a number";
+    std::cin >> num[0];
+    std::cout << num[0];
+}
