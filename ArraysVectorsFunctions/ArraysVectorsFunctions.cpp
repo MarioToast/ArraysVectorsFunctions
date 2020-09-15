5234c4242dfe2a036fe0c1b7212ee617d0d26969
Christian F <70570052+MarioToast@users.noreply.github.com>
2020-09-15 11:41:17 +0200

    d

--- ArraysVectorsFunctions/ArraysVectorsFunctions.cpp
+++ ArraysVectorsFunctions/ArraysVectorsFunctions.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
 
 int main()
@@ -16,11 +17,37 @@ int main()
     std::cin >> num[0];
     std::cout << num[0];*/
 
-    double me[100] = {};
+    /*double me[100] = {};
     std::cout << "Size of me is " << sizeof(me) / sizeof(me[0]) << "\n";
 
     for (int i = 0; i < sizeof(me) / sizeof(me[0]); i++) {
         std::cout << me[i] << " ";
+    }*/
+
+    /*double Print[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+    for (int j = 0; j < sizeof(Print) / sizeof(Print[0]); j++) {
+        std::cout << Print[j] << " ";
+        j++;
+        std::cout << Print[j] << " ";
+        j++;
+        std::cout << Print[j] << "\n";
+    }*/
+
+    /*std::vector <char> scores = {'1', '2', '3', '4', '5', '6', '7', '8', '9'};
+    for (int i = 0; i < 9; i++) {
+        if (i % 3 == 0) {
+            std::cout << "\n\n";
+        }
+        std::cout << "\t" << scores[i] << " ";
+    }*/
+
+    std::vector <char> numbers = {};
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Enter a number: " << "\n";
+        std::cin >> numbers[i];
+        std::cout << "\n";
     }
 
+    std::cout << numbers[0] << numbers[1] << numbers[2] << numbers[3] << numbers[4];
 }
